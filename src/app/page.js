4cca363cc23837434090a6f0581b4e7bb36b3ff6
Afarx <98286758+Afarx1@users.noreply.github.com>
2024-10-23 01:52:29 -0300

    Actualización a page.js
    Ajuste corto para que header.js funcione correctamente con la nueva estructura de page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,23 @@
-import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background text-white relative p-8 sm:p-20">
-      <header className="text-center text-4xl font-bold mb-8">
-        Tienda generica de objetos ficticios xd
-      </header>
+    <div className="min-h-screen bg-background text-white relative">
+      {}
+      <Header />
 
+      {/* Titulo */}
+      <div className="p-8 sm:p-20">
+        <header className="text-center text-4xl font-bold mb-8">
+          Tienda generica xd
+        </header>
+      </div>
+
+      {/* Footer */}
       <footer className="absolute bottom-4 left-4 text-sm">
         Grupo-5. Todos los derechos reservados.
       </footer>
     </div>
   );
 }
+
