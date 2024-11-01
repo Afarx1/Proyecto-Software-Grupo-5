65818ef21377efdab9e77325e07d4de5f8a94c78
vgarcuai <vicentgarcia@alumnos.uai.cl>
2024-11-01 19:53:06 -0300

    Commit 01/11 VGS
    -Agregué menú desplegable para selección de idioma en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 // components/Header.js
+"use client";
 import Link from 'next/link';
 import styles from './Header.module.css';
+import { useState } from 'react';
 
 export default function Header() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <header style={headerStyle}>
       <nav style={navStyle}>
@@ -20,6 +28,22 @@ export default function Header() {
             <Link href="register" className={styles.link}>Register</Link>
             <span className={styles.separator}>|</span>
             <Link href="/login" className={styles.link}>Login</Link>
+          </li>
+          <li style={{ ...liStyle, position: 'relative', cursor: 'pointer' }} onClick={toggleDropdown}>
+            <span className={styles.link}>Language</span>
+            {dropdownVisible && (
+              <ul style={dropdownMenuStyle}>
+                <li style={dropdownItemStyle}>
+                  <Link href="en" className={styles.link}>English</Link>
+                </li>
+                <li style={dropdownItemStyle}>
+                  <Link href="es" className={styles.link}>Español</Link>
+                </li>
+                <li style={dropdownItemStyle}>
+                  <Link href="de" className={styles.link}>Deustch</Link>
+                </li>
+              </ul>
+            )}
           </li>
            
         </ul>
@@ -52,3 +76,19 @@ const liStyle = {
   margin: '0 10px',
 };
 
+const dropdownMenuStyle = {
+  position: 'absolute',
+  top: '100%',
+  right: 0,
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+  borderRadius: '4px',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+  zIndex: 1,
+};
+
+const dropdownItemStyle = {
+  padding: '8px 0',
+  textAlign: 'right',
+};
